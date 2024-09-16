718599a9c038c6fc99c2b6fceb965f19f06a9339
Lana Kim <lanakim@buffalo.edu>
2024-09-16 18:03:07 -0400

    -mean

--- code/stuff.c
+++ code/stuff.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 float meanField(int argc, char *argv[]) {
     FILE *file = fopen(argv[argc-1], "r"); // Open the CSV file passed as the last argument
     if (file == NULL) {
         perror("Error opening file");
         return 1;
     }
-	int num = 0;
-	int totalSum = 0;
+    
+    int num = 0;
+    float totalSum = 0.0;
     char line[1024];
+
     while (fgets(line, sizeof(line), file) != NULL) {
         // Find the length of the line
         size_t len = strlen(line);
@@ -29,25 +30,34 @@ float meanField(int argc, char *argv[]) {
         while (ptr > line && *ptr != ',') {
             ptr--;
         }
-	// Print the value after the last comma
+
+        // Print the value after the last comma
         if (*ptr == ',') {
             ptr++; // Move pointer to the first character after the comma
         }
-	num+=1;
-        totalSum+=atoi(ptr);
-	
+        
+        num += 1;
+        totalSum += atof(ptr); // Use atof() to handle floating-point values
     }
 
     // Close the file
     fclose(file);
-    printf("Total%d\n",totalSum);
-    printf("Total num%d\n",num);
-    printf("%d\n",totalSum/num);
-    return totalSum/num;
+
+    if (num == 0) {
+        printf("No numeric data found.\n");
+        return 1;
+    }
+
+    float mean = totalSum / num;
+    return mean;
 }
 
-
-int main(int argc,char *argv[]){
-	meanField(argc, argv);
-    
+int main(int argc, char *argv[]) {
+	float meanVal = 0.0;
+    if(strcmp(argv[1],"-mean")!=0){
+	meanVal = meanField(argc, argv);
+	printf("%0.2f\n",meanVal);
+    }
+    return 0;
 }
+
