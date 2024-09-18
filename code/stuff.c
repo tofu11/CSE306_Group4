5ff60b7dc7a0d6cad0dc91f7dc06e9a867f6c53e
Ziang Chen <87494814+tofu11@users.noreply.github.com>
2024-09-18 14:21:42 -0400

    edited the counter printing so it would be on a new line

--- code/stuff.c
+++ code/stuff.c
@@ -1,15 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
-  FILE * inFile = NULL;
-  
+    FILE * inFile = NULL;
+    char buffer[1000];
+    char *data;
+    int counter=0;
+
     inFile = fopen(argv[argc-1], "r");  // open file for reading
     if (inFile == NULL) {
-      printf("Error opening input file\n");
       return -1;
     }
-    if (inFile == NULL) {
-    exit(EXIT_FAILURE);
+      if (fgets(buffer, sizeof(buffer), inFile) != NULL) {
+      data = strtok(buffer, ",");
+      while (data != NULL) {
+          counter++;
+          data = strtok(NULL, ",");
+      }
   }
-  }
+        else{
+        fclose(inFile);
+        return -1;
+      }
+    printf("%d\n", counter);
+    fclose(inFile);
+    return 0;
+}
+
