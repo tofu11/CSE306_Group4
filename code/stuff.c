64cf2663569062572215019fdaf244e258f4bb04
Ziang Chen <87494814+tofu11@users.noreply.github.com>
2024-09-18 16:05:58 -0400

    fixed typo

--- code/stuff.c
+++ code/stuff.c
@@ -21,22 +21,18 @@ int F_counter(FILE *inFile) {
 int main(int argc, char *argv[]) {
     FILE * inFile = NULL;
     int fcount;
-
 // open file for reading
     inFile = fopen(argv[argc-1], "r"); 
     if (inFile == NULL) {
       return -1;
 }
-
 //conditional for seeing which helper function to use
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "-f") == 0) { 
-            fcount = count_fields(inFile);
+            fcount = F_counter(inFile);
             printf("%d\n", fcount);
         }
     }
-
-
 //closing files 
     fclose(inFile);
     return 0;
