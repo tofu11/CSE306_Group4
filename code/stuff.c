dd36144f6503bd175c131f412c47d9d8ac511f95
Ziang Chen <87494814+tofu11@users.noreply.github.com>
2024-09-18 14:46:31 -0400

    edited code so it would contain if statements for when to run my code

--- code/stuff.c
+++ code/stuff.c
@@ -2,29 +2,38 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-    FILE * inFile = NULL;
+int F_counter(FILE *inFile) {
     char buffer[1000];
     char *data;
-    int counter=0;
+    int counter = 0;
+    // Read the first line from the file
+    if (fgets(buffer, sizeof(buffer), inFile) != NULL) {
+        data = strtok(buffer, ",");
+        while (data != NULL) {
+            counter++;
+            data = strtok(NULL, ",");
+        }
+    } else {
+        return -1; 
+    }
 
+    return counter;
+}
+
+int main(int argc, char *argv[]) {
+    FILE * inFile = NULL;
+    int fcount;
+    int didfhappen;
     inFile = fopen(argv[argc-1], "r");  // open file for reading
     if (inFile == NULL) {
       return -1;
+}
+    for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "-f") == 1) {
+      fcount=F_counter(inFile);
+      printf("%d\n", fcount);
     }
-      if (fgets(buffer, sizeof(buffer), inFile) != NULL) {
-      data = strtok(buffer, ",");
-      while (data != NULL) {
-          counter++;
-          data = strtok(NULL, ",");
-      }
-  }
-        else{
-        fclose(inFile);
-        return -1;
-      }
-    printf("%d\n", counter);
+}
     fclose(inFile);
     return 0;
 }
-
