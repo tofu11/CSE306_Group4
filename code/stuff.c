27664cf0672b46e668b57e4536e53aba123855b4
Ziang Chen <87494814+tofu11@users.noreply.github.com>
2024-09-18 14:18:09 -0400

    initial commit for csv and -f

--- code/stuff.c
+++ code/stuff.c
@@ -2,22 +2,14 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-
   FILE * inFile = NULL;
-
-  printf("argc: %d\n",argc);
-
-  // remember: program name is argv[0]
-  if (argc == 2) {
-    inFile = fopen(argv[1], "r");  // open file for reading
+  
+    inFile = fopen(argv[argc-1], "r");  // open file for reading
     if (inFile == NULL) {
       printf("Error opening input file\n");
+      return -1;
     }
-  }
-  else {
-    printf("Incorrect number of command line arguments.\n");
-  }
-
-  if (inFile == NULL) {
+    if (inFile == NULL) {
     exit(EXIT_FAILURE);
+  }
   }
