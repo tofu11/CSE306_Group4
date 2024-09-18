6ec74d4d2f08246e2c600b1e4492b9e6276bd170
Ziang Chen <87494814+tofu11@users.noreply.github.com>
2024-09-18 14:55:12 -0400

    debugged prev commit

--- code/stuff.c
+++ code/stuff.c
@@ -21,17 +21,16 @@ int F_counter(FILE *inFile) {
 int main(int argc, char *argv[]) {
     FILE * inFile = NULL;
     int fcount;
-    int didfhappen;
     inFile = fopen(argv[argc-1], "r");  // open file for reading
     if (inFile == NULL) {
       return -1;
 }
     for (int i = 1; i < argc; i++) {
-    if (strcmp(argv[i], "-f") == 1) {
-      fcount=F_counter(inFile);
-      printf("%d\n", fcount);
+        if (strcmp(argv[i], "-f") == 0) { 
+            fcount = count_fields(inFile);
+            printf("%d\n", fcount);
+        }
     }
-}
     fclose(inFile);
     return 0;
 }
