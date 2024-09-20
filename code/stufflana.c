3522c98ac35fed4d36274936e0ba3308dd6a2c17
Ziang Chen <87494814+tofu11@users.noreply.github.com>
2024-09-20 16:58:15 -0400

    successful merge of lana code

--- code/stufflana.c
+++ code/stufflana.c
@@ -1,70 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "helperfunc.h"
 #include <stdbool.h>
 #include <ctype.h>
-float meanField(int argc, char *argv[], bool header) {
-    FILE *file = fopen(argv[argc-1], "r"); // Open the CSV file passed as the last argument
-    if (file == NULL) {
-        printf("Error opening file");
-        return 1;
-    }
-
-    int num = 0;
-    float totalSum = 0.0;
-    char line[1024];
-
-    while (fgets(line, sizeof(line), file) != NULL) {
-        // Find the length of the line
-        size_t len = strlen(line);
-
-        // Remove newline character if present
-        if (line[len - 1] == '\n') {
-            line[len - 1] = '\0';
-            len--; // Adjust length after removing newline
-        }
-
-        // Use a pointer to traverse from the end of the line backwards
-        char *ptr = line + len - 1;
-
-        // Move pointer backwards until the last comma is found
-        while (ptr > line && *ptr != ',') {
-            ptr--;
-        }
-
-        // Print the value after the last comma
-        if (*ptr == ',') {
-            ptr++; // Move pointer to the first character after the comma
-        }
-        /*
-        if (header) {
-            header = false;
-            num -= 1;
-            totalSum -= atof(ptr);
-        }
-        */
-        
-        num += 1;
-        totalSum += atof(ptr); // Use atof() to handle floating-point values
-    }
-
-    // Close the file
-    fclose(file);
-
-    if (num == 0) {
-        printf("No numeric data found.\n");
-        return 1;
-    }
-
-    num-=1;
-
-    float mean = totalSum / num;
-    return mean;
-}
 
 
-int F_counter_2(FILE *inFile) {
+int F_counter(FILE *inFile) {
     char buffer[1000];
     char *data;
     int counter = 0;
@@ -179,7 +120,7 @@ void records(int argc, char *argv[], bool header) {
     if (argv[value][0] >= '0' && argv[value][0] <= '9') { // is it number
         field_count = atoi(argv[value]);  // convert to integer if it’s a number
     } else {
-        field_count = F_counter_2(inFile) - 1;  // if not a number, use F_counter_2 and make it a number
+        field_count = F_counter(inFile) - 1;  // if not a number, use F_counter and make it a number
     }
     rewind(inFile); // go back inFile
 
@@ -295,95 +236,22 @@ void records(int argc, char *argv[], bool header) {
 }
 
 int main(int argc, char *argv[]) {
-    int h = 0; 
-    int count = 0; 
-    char *file = NULL;
-    int r = 0;
-    int min = 0;
-    int max = 0;
-    int recordCount = 0;
-    int f = 0;
-    int f_count = 0; 
     float meanVal = 0.0;
-
-
     if(strcmp(argv[1],"-h")==0){
         if(strcmp(argv[2],"-mean")==0){
             meanVal = meanField(argc, argv, true);
             printf("%0.2f\n",meanVal);
         }
-        else if(strcmp(argv[2],"-records")==0){
+        if(strcmp(argv[2],"-records")==0){
             records(argc, argv, true);
         }
     }
- else if(strcmp(argv[1],"-mean")==0){
+    else if(strcmp(argv[1],"-mean")==0){
             meanVal = meanField(argc, argv, false);
             printf("%0.2f\n",meanVal);
         }
-        else if(strcmp(argv[1],"-records")==0){
+    else if(strcmp(argv[1],"-records")==0){
             records(argc, argv, false);
         }
-        
-    // check option
-    for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-h") == 0) {
-            h = 1;
-        } else if (strcmp(argv[i], "-r") == 0) {
-            r = 1;  
-        } else if (strcmp(argv[i], "-max") == 0) {
-            max = 1;
-        } else if (strcmp(argv[i], "-min") == 0) {
-            min = 1;
-        } else if (strcmp(argv[i], "-records") == 0) {
-            recordCount = 1;
-        }
-        else if (strcmp(argv[i], "-f") == 0) {
-            f = 1;}
-        else {
-            file = argv[argc - 1];
-        }
-    }
-
-    // check if -h is used alone
-    if (h == 1){
-        if(r == 0 && max == 0 && min == 0 && meanVal == 0.0 && recordCount == 0){
-            return EXIT_FAILURE;
-        }
-    }
-
-    if (f == 1){
-        if(r == 1 && max == 1 && min == 1 && meanVal == 1 && recordCount == 1){
-            return EXIT_FAILURE;
-        }
-    }
-    // check file
-    if (file == NULL) {
-        return EXIT_FAILURE;
-    }
-
-
-    FILE *inFile = fopen(file, "r");
-
-    // -r or -h
-    if (h) {
-        count = _h(file);
-    } 
-    
-    if (f) {       
-        count = F_counter(inFile);
-        fclose(inFile);
-    }
-    
-    if(r){
-        count = _r(file);
-    }
-
-    //print numbers
-
-    if(count == -1){
-        return EXIT_FAILURE;
-    }else{
-        printf("%d\n", count);
-    }
-    return EXIT_SUCCESS;
+    return 0;
 }
